9b4d2fbbd94574e4876133ba65b364b16f4819e8
Maria Amzil <mini.mar@hotmail.com>
2023-01-07 20:37:48 +0100

    Creating init script of Mongodb

--- init-db.js
+++ init-db.js
@@ -1,32 +1,64 @@
 const mongoose = require("mongoose");
-const Product = require("./models/product")
+const Product = require("./models/product");
+const readline = require('readline');
 
-const products = [{
-    name: "Nintendo Switch OLED",
-    onSale: true,
-    price: 280.50,
-    image: "",
-    tags: ["lifestyle"]
-}, {
-    name: "Iphone 14 Pro Max",
-    onSale: true,
-    price: 980.55,
-    image: "",
-    tags: ["lifestyle","mobile","work"]
-},{
-    name: "Apple Magic Keyboard",
-    onSale: false,
-    price: 76.80,
-    image: "",
-    tags: ["lifestyle","work"]
-},{
-    name: "Electronic Bike",
-    onSale: false,
-    price: 340.99,
-    image: "",
-    tags: ["lifestyle","motor"]
-},
-]
-Product.insertMany(products)
-.then(res => {console.log(res)})
-.catch(err => {console.log(err)})
+function askingForConfirmation(text){
+    return new Promise((resolve, reject) => {
+        const interface = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        interface.question(text, answer => {  
+            interface.close();
+            if(answer.toLowerCase() === "yes"){
+                resolve(true);
+                return;
+            }
+            resolve(false);
+        });
+    });
+};
+
+
+
+async function initProducts(){
+    const result = await Product.deleteMany();
+
+    const inserted = await Product.insertMany([{
+        name: "Nintendo Switch OLED",
+        onSale: true,
+        price: 280.50,
+        image: "",
+        tags: ["lifestyle"]
+    }, {
+        name: "Iphone 14 Pro Max",
+        onSale: true,
+        price: 980.55,
+        image: "",
+        tags: ["lifestyle","mobile","work"]
+    },{
+        name: "Apple Magic Keyboard",
+        onSale: false,
+        price: 76.80,
+        image: "",
+        tags: ["lifestyle","work"]
+    },{
+        name: "Electronic Bike",
+        onSale: false,
+        price: 340.99,
+        image: "",
+        tags: ["lifestyle","motor"]
+    },
+    ])
+}
+
+async function main(){
+    const confirmed = await askingForConfirmation("Are you sure you want to DELETE all the content from your database? [no] (Type "yes" to confirm) ")
+    if(!confirmed){
+        process.exit();
+    }
+    const connection = require("./lib/connectMongoose");
+    await initProducts();
+    connection.close();
+}
+
